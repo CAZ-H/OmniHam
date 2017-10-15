d425e03cd149061125927d28ee2c0781bcd98939
Casey Hildebrand <26800119+CAZ-H@users.noreply.github.com>
2017-10-15 14:10:08 -0500

    More functionality
    Now you can pick a dictionary from the commandline.

--- TextGen.rb
+++ TextGen.rb
@@ -1,25 +1,35 @@
-require_relative '.\markov\MarkovDictionary'
-require_relative '.\markov\MarkovSentenceGenerator'
+# Test suite for generating sentences.
+require_relative './markov/MarkovDictionary'
+require_relative './markov/MarkovSentenceGenerator'
 
-puts "-- Setting up dictionary --"
-dict = MarkovDictionary.new("regham", 2)
+puts("-- Please enter the filename without file extension of the dictionary you want to use --")
+filename = gets.strip
 
-puts "-- Setting up sentence generator --"
+puts("-- Setting up dictionary " + filename + " --")
+begin
+  dict = MarkovDictionary.new(filename, 2)
+rescue
+  puts("-- Could not find dictionary --")
+  exit
+end
+
+puts("-- Setting up sentence generator --")
 gen = MarkovSentenceGenerator.new(dict, 100, 50000)
 
-puts "-- Ready --"
+puts("-- Input text to generate sentences. q to quit. --")
+puts("-- Ready --")
 input = gets
 while input != "q\n" do
-  puts "-- Generating sentences --"
+  puts("-- Generating sentences --")
   if input.strip == ""
     for i in 1..5 do
-      puts gen.generate_random(50)
+      puts(gen.generate_random(50))
   end
   else
     for i in 1..5 do
-      puts gen.generate_containing(input.strip, 50)
+      puts(gen.generate_containing(input.strip, 50))
     end
   end
-
+  puts("-- Ready --")
   input = gets
 end
